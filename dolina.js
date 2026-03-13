3e2489d12b6113bebe31e2fcc741c30a6485aa18
vi2tik <vitutik90ua@ukr.net>
2026-03-13 15:05:26 +0200

    Create dolina.js

---
+++ dolina.js
@@ -0,0 +1,12 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.type = 'text/javascript';
+  script.src = "https://volnorez.com/plugins/jscode/player_js/001d180b/%7B%22autoplay%22%3Afalse%2C%22share%22%3A%22source%22%2C%22skin%22%3A%22main%22%7D";
+  var container = document.getElementById('vplayer001d180b');
+  if (container) {
+    container.appendChild(script);
+  } else {
+    console.warn("Контейнер #vplayer001d180b не знайдено");
+  }
+})();
